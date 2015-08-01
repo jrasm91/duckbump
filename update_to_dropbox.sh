8533b360ea0d9cd5303cba92005add4a1e9c3315
Jason Rasmussen <jrasm91@gmail.com>
2015-08-01 00:54:43 -0700

    auto generated update

--- update_to_dropbox.sh
+++ update_to_dropbox.sh
@@ -1,3 +1,3 @@
-git add . && git commit -m "auto generated update" && git push
+git add --all && git commit -m "auto generated update" && git push
 firebase deploy
 cp -r /home/jrasm91/Development/Web/duckbump/* /home/jrasm91/Dropbox/Jason/Duckbump/
