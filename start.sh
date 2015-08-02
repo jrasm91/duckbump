a4e2be16b93779445ec7f0c539f0708cd83829a9
Jason Rasmussen <jrasm91@gmail.com>
2015-08-01 17:04:30 -0700

    auto generated update

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-cd /home/jrasm91/Development/Web/duckbump/
-python3 -m http.server 8000; 
+cd /home/jrasm91/Development/Web/duckbump
+python3 -m http.server 8000&
+cd /home/jrasm91/Dropbox/Jason/Duckbump
+python3 -m http.server 8001& 
