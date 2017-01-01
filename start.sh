35f585634fc27e36135d5b4ace0756b465a25f72
Jason Rasmussen <jrasm91@gmail.com>
2017-01-01 14:21:30 -0700

    removed unused files

--- start.sh
+++
@@ -1,4 +0,0 @@
-cd /home/jrasm91/Development/Web/duckbump
-python3 -m http.server 8000&
-cd /home/jrasm91/Dropbox/Jason/Duckbump
-python3 -m http.server 8001& 
--- update_to_dropbox.sh
+++
@@ -1,2 +0,0 @@
-cp -r /home/jrasm91/Dropbox/Jason/Duckbump/img/* /home/jrasm91/Development/Web/duckbump/img/
-cp -r /home/jrasm91/Development/Web/duckbump/js/* /home/jrasm91/Dropbox/Jason/Duckbump/js/
